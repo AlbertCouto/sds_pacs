784fe97535bca6b010cefafc660123f25e08aa7b
SamuGB <43610968+SamuGB@users.noreply.github.com>
2019-01-10 16:20:44 +0100

    Cifrado/GenerFicheros

---
+++ RepublicSystem_FNATIK/ActualizarCifrado/ActualizarCifrado/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ActualizarCifrado
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            CifrarLetras.Cifrado cifrado = new CifrarLetras.Cifrado();
+            AccesoBBDD.AccesoBBDD acceso_bd = new AccesoBBDD.AccesoBBDD();
+            string query = "insert into InnerEncryption (Day, Active) values ('" + DateTime.Today.ToString("yyyy-MM-dd") + "', 1)";
+            acceso_bd.Insert(query);
+
+            foreach (KeyValuePair<char, string> item in cifrado.GenerarCifrado())
+            {
+                query = "insert into InnerEncryptionData (IdInnerEncryption ,Word, Numbers, OrderData) values ((select max(idInnerEncryption) from InnerEncryption),'" + item.Key + "','" + item.Value + "', 1)";
+                acceso_bd.Insert(query);
+            }
+
+            MessageBox.Show("Todo OK");
+        }
+    }
+}
---
+++ RepublicSystem_FNATIK/Threads/Threads/Threads.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Threads
+{
+    public class Threads
+    {
+        //private string ruta = "C:\\Users\\admin\\Desktop\\RepublicSystem_FNATIK\\Threads\\Threads\\resources\\pacs";
+        private char[] abecedario = new char[] {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        public void GenerarFicheros()
+        {
+            MessageBox.Show("Generando Ficheros Base...");
+            Parallel.Invoke(() => { Generar("1"); },
+                            () => { Generar("2"); },
+                            () => { Generar("3"); },
+                            () => { Generar("4"); }
+                            );
+            MessageBox.Show("Ficheros Base Generados");
+        }
+        private void Generar(string valor)
+        {
+            string ruta = "C:\\Users\\admin\\Desktop\\RepublicSystem_FNATIK\\Threads\\Threads\\resources\\pacs" + valor + ".txt";
+            if (!File.Exists(ruta))
+            {
+                Random rnd = new Random();
+                using (StreamWriter sw = File.CreateText(ruta))
+                {
+                    for (int j = 0; j < 1000000; j++)
+                    {
+                        int numero = rnd.Next(abecedario.Length);
+                        char letra = abecedario[numero];
+                        sw.Write(letra);
+                    }
+                }
+            }
+        }
+        public void Encriptar()
+        {
+
+        }
+        public void Desencriptar()
+        {
+
+        }
+    }
+}
