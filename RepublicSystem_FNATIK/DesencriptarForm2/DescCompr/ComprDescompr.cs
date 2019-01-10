a1e0312d53b21d8f1b8a7ddab7cf63256020888c
Marcus Hergo <marc.hernandez@sarria.salesians.cat>
2019-01-10 18:13:48 +0100

    des
    Descenriptar form

---
+++ RepublicSystem_FNATIK/DesencriptarForm2/DescCompr/ComprDescompr.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescCompr
+{
+    public class ComprDescompr
+    {
+        //Ruta on estan els arxius
+        string startPath;
+        //startPath = @"C:\Users\admin\Desktop\start";
+
+        //Ruta on va el zip + nom del zip (Tant si existeix com si no)
+        string zipPath;
+        //zipPath = @"C:\Users\admin\Desktop\result.zip";
+
+        //Ruta on està el ZIP comprimit
+        string zipPathCompr;
+
+        //Ruta on es descomprimeix el zip
+        string extractPath;
+
+
+
+        public void Comprimir(string startPath, string zipPath)
+        {
+            if (!System.IO.File.Exists(zipPath))
+            {
+                ZipFile.CreateFromDirectory(startPath, zipPath);
+            }
+        }
+
+
+        public void Descomprimir(string zipPath, string extractPath)
+        {
+
+                ZipFile.ExtractToDirectory(zipPath, extractPath);
+            
+        }
+    }
+}
---
+++ RepublicSystem_FNATIK/DesencriptarForm2/DesencriptarForm2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DescCompr;
+using System.Windows.Forms;
+
+
+namespace DesencriptarForm2
+{
+    public partial class Form1 : Form
+    {
+        ComprDescompr comprDescompr = new ComprDescompr();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string zipPath = @"C:\Users\admin\Desktop\Desktop.zip";
+            string extractPath = @"C:\Users\admin\Desktop";
+            comprDescompr.Descomprimir(zipPath, extractPath);
+            MessageBox.Show("DESCOMPRIMIT");
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
