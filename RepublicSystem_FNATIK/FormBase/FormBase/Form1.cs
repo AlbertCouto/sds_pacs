d321dd81305083d55d9cb82d703972ba3421fbb9
Marcus Hergo <marc.hernandez@sarria.salesians.cat>
2019-01-16 16:29:53 +0100

    form base

---
+++ RepublicSystem_FNATIK/FormBase/FormBase/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormBase
+{
+    public partial class Form1 : Form
+    {
+
+        int timeLeft;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                // Display the new time left
+                // by updating the Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show
+                // a MessageBox, and fill in the answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+    
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+        }
+    }
+}
