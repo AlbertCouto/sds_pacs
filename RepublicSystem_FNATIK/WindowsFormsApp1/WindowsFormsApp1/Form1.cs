96a1d3ed72993d01c5e26cb18728ea6aa2cb442c
Toonii <toni.arandasds@gmail.com>
2019-01-22 16:10:08 +0100

    Cliente to flama

---
+++ RepublicSystem_FNATIK/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cliente_Nave.Class1 si = new Cliente_Nave.Class1();
+            si.puerto = 5678;
+            si.Start_Client();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cliente_Nave.Class1 si = new Cliente_Nave.Class1();
+            si.puerto = 9250;
+            si.Start_Client();
+        }
+    }
+}
