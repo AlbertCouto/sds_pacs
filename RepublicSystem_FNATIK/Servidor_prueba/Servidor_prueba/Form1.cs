7feb54014024f30148aa9bcabd5bf4076afa1369
AlbertCouto <aguetta12@gmail.com>
2019-01-22 16:06:06 +0100

    los puto traterrestres

---
+++ RepublicSystem_FNATIK/Servidor_prueba/Servidor_prueba/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Servidor_Nave;
+
+namespace Servidor_prueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            Servidor_Nave.Class1 servidor = new Class1();
+            label1.Visible = true;
+            servidor.StartServer();            
+
+        }
+    }
+}
