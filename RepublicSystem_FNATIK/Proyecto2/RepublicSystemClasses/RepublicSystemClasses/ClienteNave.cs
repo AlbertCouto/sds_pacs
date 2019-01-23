644431485efafc06ebd457c6fb03f3e1a320699b
AlbertCouto <aguetta12@gmail.com>
2019-01-23 16:51:26 +0100

    cosas bien

--- RepublicSystem_FNATIK/Proyecto2/RepublicSystemClasses/RepublicSystemClasses/ClienteNave.cs
+++ RepublicSystem_FNATIK/Proyecto2/RepublicSystemClasses/RepublicSystemClasses/ClienteNave.cs
@@ -15,7 +15,7 @@ namespace RepublicSystemClasses
         public void Start_Client()
         {
             string SendingFilePath = "C:\\Users\\admin\\Desktop\\as.rar";
-            string IP = ((bd.PortarPerConsulta("select IPPlanet from Planets where idPlanet = 1")).Tables[0].Rows[0][1]).ToString();
+            string IP = ((bd.PortarPerConsulta("select IPPlanet from Planets where idPlanet = 3")).Tables[0].Rows[0][1]).ToString();
             puerto = (Int32)((bd.PortarPerConsulta("select PortPlanetFile from Planets where idPlanet = 1")).Tables[0].Rows[0][1]);
             SendTCP(SendingFilePath, IP, puerto);
         }
