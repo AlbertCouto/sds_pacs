2b7577c06fc66cac6e613303f6c47ce4b50f18fa
SamuGB <43610968+SamuGB@users.noreply.github.com>
2019-01-24 17:43:22 +0100

    iep

--- RepublicSystem_FNATIK/Proyecto2/RepublicSystemClasses/RepublicSystemClasses/GenerarMensajes.cs
+++ RepublicSystem_FNATIK/Proyecto2/RepublicSystemClasses/RepublicSystemClasses/GenerarMensajes.cs
@@ -14,22 +14,22 @@ namespace RepublicSystemClasses
         public string GenerarMensajeInicio()
         {
             string fecha, fecha_bien;
-            string[] cosas;
-            string[] cosas2;
+            string[] fecha_split1;
+            string[] fecha_split2;
             string SSID;
             string DeliveryID;
 
 
             ds = db.PortarPerConsulta("select Day from InnerEncryption where idInnerEncryption = (select max(idInnerEncryption) from InnerEncryption);");
             fecha = (ds.Tables[0].Rows[0][0]).ToString();
-            cosas = fecha.Split(' ');
-            cosas2 = cosas[0].Split('/');
+            fecha_split1 = fecha.Split(' ');
+            fecha_split2 = fecha_split1[0].Split('/');
 
             ds = db.PortarPerConsulta("select SpaceShip, CodeDelivery from DeliveryData");
             SSID = (ds.Tables[0].Rows[0][0]).ToString();
             DeliveryID = (ds.Tables[0].Rows[0][1]).ToString();
 
-            fecha_bien = cosas2[1] + cosas2[0] + cosas2[2] + SSID + DeliveryID;
+            fecha_bien = fecha_split2[1] + fecha_split2[0] + fecha_split2[2] + SSID + DeliveryID;
 
             return fecha_bien; 
         }
