79d62960cf27a39ae0fb11c8f2e24ab82e3dd08a
AlbertCouto <aguetta12@gmail.com>
2019-02-05 16:03:02 +0100

    limpieza

--- RepublicSystem_FNATIK/Proyecto2/RepublicSystemClasses/RepublicSystemClasses/ClasePlaneta.cs
+++ RepublicSystem_FNATIK/Proyecto2/RepublicSystemClasses/RepublicSystemClasses/ClasePlaneta.cs
@@ -57,13 +57,12 @@ namespace RepublicSystemClasses
                 Console.WriteLine(ex.Message);
             }
             byte[] RecData = new byte[BufferSize];
-            int RecBytes;
+            
             byte[] RecData2 = new byte[BufferSize];
 
             for (; ; )
             {
-                TcpClient client = null;
-                NetworkStream netstream = null;
+                
                 TcpClient client2 = null;
                 NetworkStream netstream2 = null;
                 TcpClient client3 = null;
@@ -72,19 +71,7 @@ namespace RepublicSystemClasses
                 Status = string.Empty;
                 try
                 {
-                    if (Listener.Pending())
-                    {
-                        client = Listener.AcceptTcpClient();
-                        netstream = client.GetStream();
-                        string ruta = "C:\\Users\\admin\\Desktop\\Pruebas.rar";
-                        FileStream Fs = new FileStream(ruta, FileMode.OpenOrCreate, FileAccess.Write);
-                        RecBytes = netstream.Read(RecData, 0, RecData.Length);
-                        Fs.Write(RecData, 0, RecBytes);
-                        MessageBox.Show("Archivo recibido");
-                        Fs.Close();
-                        netstream.Close();
-                        client.Close();
-                    }
+                   
                     if (Listener2.Pending())
                     {
                         client2 = Listener2.AcceptTcpClient();
