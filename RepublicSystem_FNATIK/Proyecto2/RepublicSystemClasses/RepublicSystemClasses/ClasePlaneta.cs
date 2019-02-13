69a8bdaaeb6201b4d421c8b5f74928796ee90ae4
AlbertCouto <aguetta12@gmail.com>
2019-02-13 16:16:26 +0100

    ASD

--- RepublicSystem_FNATIK/Proyecto2/RepublicSystemClasses/RepublicSystemClasses/ClasePlaneta.cs
+++ RepublicSystem_FNATIK/Proyecto2/RepublicSystemClasses/RepublicSystemClasses/ClasePlaneta.cs
@@ -55,7 +55,7 @@ namespace RepublicSystemClasses
         {
             int puerto_archivo = Convert.ToInt32((bd.PortarPerConsulta("select PortPlanetFile from Planets where idPlanet = 1")).Tables[0].Rows[0][0]);
             int puerto_mensaje = Convert.ToInt32((bd.PortarPerConsulta("select PortPlanetText from Planets where idPlanet = 1")).Tables[0].Rows[0][0]);
-            string IP = ((bd.PortarPerConsulta("select IPSpaceShip from SpaceShips where idSpaceShip = 2")).Tables[0].Rows[0][0]).ToString();
+            string IP = ((bd.PortarPerConsulta("select IPSpaceShip from SpaceShips where idSpaceShip = 1")).Tables[0].Rows[0][0]).ToString();
 
             ReceiveTCP(puerto_archivo, puerto_mensaje, IP);
            
