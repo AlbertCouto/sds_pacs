b46d2c8fc3339efc270331a442cf9258ad85be8a
SamuGB <43610968+SamuGB@users.noreply.github.com>
2019-02-18 20:59:23 +0100

    ánimo

---
+++ RepublicSystem_FNATIK/Proyecto2/IntroNave/IntroNave/IntroForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace IntroNave
+{
+    partial class IntroForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(258, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(59, 113);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(166, 13);
+            this.Label.TabIndex = 1;
+            this.Label.Text = "Escoja el Planeta al que se dirige:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 244);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ RepublicSystem_FNATIK/Proyecto2/IntroNave/IntroNave/IntroForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using RepublicSystemClasses;
+
+namespace IntroNave
+{
+    public partial class IntroForm : Form
+    {
+        public IntroForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LlenarCombobox();
+            }
+            catch
+            {
+                MessageBox.Show("No se ha encontrado ningun Planeta");
+            }
+        }
+        private void LlenarCombobox()
+        {
+            button1.Enabled = false;
+            AccesoBD db = new AccesoBD();
+            DataTable dt = new DataTable();
+            string query = "select DescPlanet from Planets";
+            dt = db.PortarPerConsulta(query).Tables[0];
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+                comboBox1.Items.Add(dt.Rows[i][0].ToString());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form Nave = new Form();
+            if (comboBox1.SelectedItem.ToString() == "Las Cacas") Nave = new NaveForm.NaveForm();
+            else Nave = new NaveOuter.NaveOuterForm();
+            Nave.Show();
+        }
+    }
+}
